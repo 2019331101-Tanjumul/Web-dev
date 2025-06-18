0f773a98a5216af31abb517b6d0bfa86258205e4
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-06-18 17:01:43 +0600

    use of arrow in fucntion

--- JavaScript/first_function.js
+++ JavaScript/first_function.js
@@ -4,7 +4,7 @@ function myFunction(p1 = 3, p2 = 4) {
     return p1 * p2; 
 }
 
-console.log(myFunction());       // uses default values: 3 * 4 = 12
+// console.log(myFunction());       // uses default values: 3 * 4 = 12
 console.log(myFunction(5, 6));   // outputs: 30
 
 
@@ -17,3 +17,17 @@ console.log(myFunction());
 console.log(cricK());
 
 console.log(myFunction() * cricK());
+
+// here is the arrowed version 
+
+console.log("Arrow funciton"); 
+
+const myfunction = (p1=5 , p2=4) => p1*p2; 
+
+const chicken = (cooker = 6 , flame = 20) => cooker / flame; 
+
+console.log(myfunction()); 
+console.log(chicken()); 
+
+console.log(myfunction()*chicken()); 
+
