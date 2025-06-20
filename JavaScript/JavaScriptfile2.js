c4f6f3f01026aceef20b232f570203a5eceef6d0
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-06-20 17:37:12 +0600

    string with different functions

--- JavaScript/JavaScriptfile2.js
+++ JavaScript/JavaScriptfile2.js
@@ -18,3 +18,26 @@ console.log($myname); //valid will work also !
 let tanj_umul = "IS a snake case !"; 
 
 console.log(tanj_umul); 
+
+const pi  =3.1416 ;
+
+console.log(pi*2*3); 
+
+let firstname = " Tanjumul Alom Siam "; 
+
+console.log(firstname[7-2]); // using the string indexing 
+
+console.log(firstname.length); 
+
+console.log(firstname[firstname.length-2]);
+
+//using trim method to wipe out the spaces in the string 
+
+let firstnames = "     tanju     "; 
+console.log(firstnames.length); 
+
+firstnames = firstnames.trim(); 
+console.log(firstnames); 
+console.log(firstnames.length);
+
+
