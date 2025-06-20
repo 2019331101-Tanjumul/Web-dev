1ac53d1560e9175b202683dc44ffc8019e338421
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-06-20 17:17:59 +0600

    accepted variables and types

--- JavaScript/JavaScriptfile2.js
+++ JavaScript/JavaScriptfile2.js
@@ -1,3 +1,20 @@
 console.log("Tanjumul"); 
 console.log("SUST"); 
 console.log(`also will work`); 
+
+
+var myName = "tanjumul"; 
+
+console.log(myName);
+
+let $myname = " alos tanju"; 
+
+console.log($myname); //valid will work also ! 
+
+//!valid #mywork = "tanju"; 
+
+// cameLcase and SnakeCase 
+
+let tanj_umul = "IS a snake case !"; 
+
+console.log(tanj_umul); 
