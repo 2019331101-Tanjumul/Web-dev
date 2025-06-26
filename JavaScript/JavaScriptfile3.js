898ac321100e57006616c81c9eeed3b4b54bece4
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-06-26 22:45:46 +0600

    change in index file

---
+++ JavaScript/JavaScriptfile3.js
@@ -0,0 +1,26 @@
+
+//Nested if else 
+
+// The game
+// wining number = 19 
+
+// 19 your guess is right 
+// 17 too low 
+// 20 too high
+
+
+let winNum  = 16; 
+
+if (winNum === 19){ 
+    console.log("Your guess is right");
+}
+ else if (winNum === 17) { 
+    console.log(" 17 is too low");
+ }
+  else if (winNum === 20 ){ 
+    console.log(" 20 is too high"); 
+  }
+  else {
+    console.log("you have guessed a wrong number"); 
+}
+
